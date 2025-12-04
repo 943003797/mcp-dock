9094f24575bdecba4085bc19238cd924e290cf29
Kinson <943003797@qq.com>
2025-12-04 16:51:22 +0800

    fix(Win): 添加远程调试地址参数到chrome启动脚本

--- Win/start-chrome.bat
+++ Win/start-chrome.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="%TEMP%\chrome-profile-stable"
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --user-data-dir="%TEMP%\chrome-profile-stable"
 pause
