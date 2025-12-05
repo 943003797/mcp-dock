1fa9770ebddffe62f0de0a4b7adea5bba04d4169
Kinson <943003797@qq.com>
2025-12-05 15:16:44 +0800

    fix(Win): 修正启动chrome脚本引用的ps1文件名错误

--- Win/start-chrome.bat
+++ Win/start-chrome.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell -NoProfile -Command "Start-Process powershell.exe -ArgumentList '-NoLogo -ExecutionPolicy Bypass -File \"%~dp0start-chrome.ps1\"' -Verb RunAs"
+powershell -NoProfile -Command "Start-Process powershell.exe -ArgumentList '-NoLogo -ExecutionPolicy Bypass -File \"%~dp0chrome-port-config.ps1\"' -Verb RunAs"
 
 "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --user-data-dir="%TEMP%\chrome-profile-stable"
 pause
