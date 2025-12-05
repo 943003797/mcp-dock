6fa98bd5a76c150990ef31cd1e95a7a70945e4ac
Kinson <943003797@qq.com>
2025-12-05 15:19:05 +0800

    refactor(Win): 简化 Chrome 本地启动脚本
    移除端口转发和防火墙规则配置，仅保留 Chrome 启动命令

--- Win/start-chrome-local.bat
+++ Win/start-chrome-local.bat
@@ -1,7 +1,2 @@
-netsh interface portproxy add v4tov4 `
-  listenport=9223 listenaddress=0.0.0.0 `
-  connectport=9222 connectaddress=127.0.0.1
-New-NetFirewallRule -DisplayName "ChromeDevTools-9223" `
-  -Direction Inbound -Protocol TCP -LocalPort 9223 -Action Allow
 "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --user-data-dir="%TEMP%\chrome-profile-stable"
 pause
